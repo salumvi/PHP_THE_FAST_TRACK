ff3b3d2d39143584952aa1f400a5fec30c1ea106
Juan Pedro <salumvi@gmail.com>
2021-01-08 08:33:05 +0100

    Paso 5

--- public/index.php
+++ public/index.php
@@ -23,5 +23,8 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+
+dump($request); // barra lateral para depurar
+
 $response->send();
 $kernel->terminate($request, $response);
