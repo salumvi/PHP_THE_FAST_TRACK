86aa89d0c15c6943453e8a10f49c79b5f5023fad
Juan Pedro <salumvi@gmail.com>
2021-02-20 21:09:22 +0100

    Fin del puto 17

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+
+    private $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    
+    public function load(ObjectManager $manager)
+    {
+                $amsterdam = new Conference();
+                $amsterdam->setCity('Amsterdam');
+                $amsterdam->setYear('2019');
+                $amsterdam->setIsInternational(true);
+                $amsterdam->setSlug('Amsterdam-2019');
+
+                $manager->persist($amsterdam);
+        
+                $paris = new Conference();
+                $paris->setCity('Paris');
+                $paris->setYear('2020');
+                $paris->setIsInternational(false);
+                $paris->setSlug('Paris-2020');
+                $manager->persist($paris);
+        
+                $comment1 = new Comment();
+                $comment1->setConference($amsterdam);
+                $comment1->setAuthor('Fabien');
+                $comment1->setEmail('fabien@example.com');
+                $comment1->setText('This was a great conference.');
+                $manager->persist($comment1);
+
+                $admin = new Admin();
+                $admin->setRoles(['ROLE_ADMIN']);
+                $admin->setUsername('admin');
+                $admin->setPassword($this->encoderFactory->getEncoder(Admin::class)->encodePassword('admin', null));
+                $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
